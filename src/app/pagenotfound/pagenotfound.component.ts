6a4c251085ebb201fffac8452778ca59347acbeb
Fabrício Oliveira <fabricio_paula@hotmail.com>
2018-08-20 16:03:46 -0300

    adding "back" function

--- src/app/pagenotfound/pagenotfound.component.ts
+++ src/app/pagenotfound/pagenotfound.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 @Component({
     selector: 'not-found',
-    template: '<p>Página não encontrada</p> <img src="http://i.giphy.com/LHZyixOnHwDDy.gif"> '
+    template: '<p>Página não encontrada</p> <img src="http://i.giphy.com/LHZyixOnHwDDy.gif" (click)="voltar()"> '
 })
 
 export class PageNotFoundComponent{
+
+constructor (private location: Location){
+
+}
+
+ngOnInit() : void {
+    console.log('url atual: ', document.location.href);
+    console.log('location: ', location.href);
+    
+}
+
+voltar() : void {
+    this.location.back();
+    
+}
 
 }
