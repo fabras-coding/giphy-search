efa581726c98a9122eb9b0da37076741a5c1f7f7
Fabricio de Paula Oliveira <fabricio_paula@hotmail.com>
2018-11-24 20:12:46 -0200

    a

--- src/app/pipe/pipe.module.ts
+++ src/app/pipe/pipe.module.ts
@@ -3,12 +3,13 @@ import { ShareModuleModule } from "../share/share.module";
 import { PipeComponent } from "./pipe.component";
 import { GiphyPipeRoutingModule } from "./pipe-routing.module";
 import { TipoPessoaPipe } from "../share/pipe/tipo-pessoa.pipe";
+import { CapitalizePipe } from "../share/pipe/capitalize.pipe";
 
 
 @NgModule({
     imports: [ShareModuleModule, GiphyPipeRoutingModule],
     exports: [PipeComponent],
-    declarations: [PipeComponent, TipoPessoaPipe]
+    declarations: [PipeComponent, TipoPessoaPipe, CapitalizePipe]
 
 })
 
